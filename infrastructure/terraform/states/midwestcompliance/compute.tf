99008c226c78c3dd486436081ab32d6260a720bb
Tyler Kellen <tyler@sleekcode.net>
2017-05-29 22:08:45 -0400

    disable staging server until needed

--- infrastructure/terraform/states/midwestcompliance/compute.tf
+++ infrastructure/terraform/states/midwestcompliance/compute.tf
@@ -12,16 +12,16 @@ resource "aws_instance" "production" {
   iam_instance_profile = "${module.iam.instance_profile}"
 }
 
-resource "aws_instance" "staging" {
-  ami = "${data.aws_ami.ubuntu.id}"
-  instance_type = "t2.medium"
-  key_name = "default"
-  subnet_id = "${element(module.subnet.ids,0)}"
-  vpc_security_group_ids = [
-    "${aws_security_group.main.id}",
-  ]
-  tags {
-    Name = "${var.name}-staging"
-  }
-  iam_instance_profile = "${module.iam.instance_profile}"
-}
+#resource "aws_instance" "staging" {
+#  ami = "${data.aws_ami.ubuntu.id}"
+#  instance_type = "t2.medium"
+#  key_name = "default"
+#  subnet_id = "${element(module.subnet.ids,0)}"
+#  vpc_security_group_ids = [
+#    "${aws_security_group.main.id}",
+#  ]
+#  tags {
+#    Name = "${var.name}-staging"
+#  }
+#  iam_instance_profile = "${module.iam.instance_profile}"
+#}
