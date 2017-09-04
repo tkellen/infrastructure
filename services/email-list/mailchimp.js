711a35218e78370246a6eb78e051f5b654456823
Tyler Kellen <tyler@sleekcode.net>
2017-09-04 19:41:55 -0400

    fleshing out email-list service

--- services/email-list/mailchimp.js
+++ services/email-list/mailchimp.js
@@ -21,46 +21,84 @@ async function status(payload) {
 }
 
 async function subscribe(payload) {
-  let state = 'pending';
-  let response = 'waiting-on-approval';
-  let existingMember = {
-    interests: {}
-  }
+  // By default we are expecting users are first time visitors. This means
+  // we will put them into a "pending" state. That will tell Mailchimp to
+  // send them a "double opt-in" email.
+  let state = 'pending'
 
+  // By default our message should be that we are waiting on approval from
+  // the user. This corresponds to the pending scenario described above.
+  let message = 'waiting-on-approval'
+
+  // By default we want to use the HTTP method put to update/create in place.
+  let method = 'put';
+
+  // By default we assume no error has occured.
+  let error = false;
+
+  // Keep member data in scope so we can return it
+  let member = {};
+
+  // First, let's see if this email is already in our list.
   try {
-    // if someone is already in the system for any reason and they are
-    // hitting this method we can safely assume they want to be subscribed
-    // and just set them to that state.
-    existingMember = await status(payload)
+    member = await status(payload)
     if (member) {
+      // If a user is already on the list and they are signing up again, we can
+      // safely assume they want to be subscribed. No matter what their previous
+      // state was, make them be "subscribed".
       state = 'subscribed'
-      response = 'welcome-back'
+
+      // If a user already exists, change to updating specific fields rather
+      // than whole sale replacment/creation.
+      method = 'patch';
+
+      // We want to be smart in our reply. If a user has already signed up and
+      // they are back for more, let's welcome them vs showing them the same
+      // initial signup flow.
+      message = 'welcome-back'
     }
   } catch(e) {
-    // we don't care if this fails
+    // If this fails it just means the user didn't exist.
   }
 
+  // Prep the payload we are going to use for creating/updating our user
+  const body = {
+    email_address: payload.email_address,
+    status: state,
+    merge_fields: {
+      FNAME: payload.first_name
+    },
+    // We use hidden groups to classify how people signed up. This ensures
+    // that our signup call will flag the user as being in any group that
+    // we havs specified.
+    interests: payload.group_id ? { [payload.group_id]: true } : {}
+  }
+  // Create or update the user!
   try {
-    const member = await client().put({
+    member = await client()[method]({
       path: `/lists/${payload.list_id}/members/${md5(payload.email_address)}`,
-      body: {
-        email_address: payload.email_address,
-        status: state,
-        merge_fields: {
-          FNAME: payload.first_name
-        }
-        interests: Object.assign(member.)
-      }
+      body: body
     })
   } catch(e) {
+    error = true
     if (e.status === 404) {
-      response = 'failed-bad-list'
-    } else if (e.detail === 'Please provide a valid email address.') {
-      response = 'failed-bad-email'
+      message = 'failed-bad-list'
+    } else if (e.detail.match(/provide a valid email/)) {
+      message = 'failed-bad-email'
+    } else if (e.detail.match(/interest ID/)) {
+      // TODO: handle this by just removing the group assignment if it
+      // doesn't exist
+      message = 'failed-bad-group'
     }
   }
-
-  return response;
+  return {
+    error,
+    message,
+    first_name,
+    email_address,
+    list_id: member.list_id,
+    group_ids: member.interests
+  }
 }
 
 exports.subscribe = subscribe
--- services/email-list/test-index.js
+++ services/email-list/test-index.js
@@ -3,19 +3,34 @@ const micro = require('micro')
 const listen = require('test-listen')
 const service = micro(require('./'))
 const request = require('request-promise')
-const { cleanup, mockdata } = require('./testhelpers');
+const { withMailchimp, mockEmail } = require('./testhelpers');
 
 test('adding emails to mailchimp', async t => {
-  t.pass()
-  //const payload = mockdata()
-  //const url = await listen(service)
-  //const response = await request({
-  //  method: 'POST',
-  //  uri: url,
-  //  body: payload,
-  //  json: true,
-  //  resolveWithFullResponse: true
-  //})
-  //t.deepEqual(response.body, payload)
-  //service.close()
+  const url = await listen(service)
+  const email = mockEmail()
+  await withMailchimp(async ({list_id, group_id_one, group_id_two}) => {
+    try {
+      const response = await request({
+        method: 'POST',
+        uri: url,
+        body: {
+          list_id: list_id,
+          group_id: group_id_one,
+          email_address: email,
+          first_name: 'test'
+        },
+        json: true,
+        resolveWithFullResponse: true
+      })
+    } catch (e) {
+      throw e;
+    }
+    t.pass()
+  })
+  service.close()
+/*
+
+  } catch (e) {
+    console.log(e)
+  }*/
 })
