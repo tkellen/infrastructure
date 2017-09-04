66bb1e8ed85fcf56ede925bba8f33a5aa9eba42e
Tyler Kellen <tyler@sleekcode.net>
2017-09-04 11:08:00 -0400

    add email support for aevitas.io

--- infrastructure/terraform/states/core/dns.tf
+++ infrastructure/terraform/states/core/dns.tf
@@ -12,3 +12,19 @@ resource "aws_route53_record" "cname-cdn" {
   ttl = "1"
   records = ["${aws_cloudfront_distribution.cdn.domain_name}"]
 }
+
+resource "aws_route53_record" "mx" {
+  zone_id = "${aws_route53_zone.main.id}"
+  type = "MX"
+  name = "${var.domain}"
+  ttl = "1"
+  records = [
+    "20 alt1.aspmx.l.google.com.",
+    "20 alt2.aspmx.l.google.com.",
+    "30 aspmx2.googlemail.com.",
+    "30 aspmx3.googlemail.com.",
+    "30 aspmx4.googlemail.com.",
+    "30 aspmx5.googlemail.com.",
+    "10 aspmx.l.google.com."
+  ]
+}
